f93c8abf820f08fe2be4c6de3b0c7120d238db4c
Ester Oliveira <86311735+esteroliver@users.noreply.github.com>
2022-07-23 21:33:04 -0300

    hh1

--- primeiraetapa/src/App.js
+++ primeiraetapa/src/App.js
@@ -2,25 +2,30 @@ import './App.css';
 import axios from 'axios'
 
 function App() {
+  Element.innerHTML = 
   const alive = axios.get('https://doar-computador-api.herokuapp.com/')
-  if (alive===true){
-    return (
-      <div>
-        <main>
-          <p>API Online</p>
-        </main>
-      </div>
-    )
-  }
+    
+    if (alive===true){
+      return (
+        <div>
+          <main>
+            <h1 id='titulo'></h1>
+            <p>API Online</p>
+          </main>
+        </div>
+      )
+    }
   else{
-    return (
-      <div>
-        <main>
-          <p>API Offline</p>
-        </main>
-      </div>
-    )
+      return (
+        <div>
+          <main>
+            <h1>Doação de computadores usados</h1>
+            <p>API Offline</p>
+          </main>
+        </div>
+      )
+    }
+  
   }
-}
 
 export default App;
